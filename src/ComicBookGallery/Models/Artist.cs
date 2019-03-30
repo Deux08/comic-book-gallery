5ef44cdecd6ad87d86466a231242b0bae56a83b8
Ibrahim Alrasyid Waterkamp <s10188377@connect.np.edu.sg>
2019-03-30 14:14:26 +0800

    Added ComicBook and Artist Models

---
+++ src/ComicBookGallery/Models/Artist.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComicBookGallery.Models
+{
+    public class Artist
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private string role;
+        public string Role
+        {
+            get { return role; }
+            set { role = value; }
+        }
+
+    }
+}
---
+++ src/ComicBookGallery/Models/ComicBook.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComicBookGallery.Models
+{
+    public class ComicBook
+    {
+        private int id;
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        private string seriesTitle;
+        public string SeriesTitle
+        {
+            get { return seriesTitle; }
+            set { seriesTitle = value; }
+        }
+
+        private int issueNumber;
+        public int IssueNumber
+        {
+            get { return issueNumber; }
+            set { issueNumber = value; }
+        }
+
+        private string description;
+        public string DescriptionHtml
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+        private Artist[] artists;
+        public Artist[] Artists
+        {
+            get { return artists; }
+            set { artists = value; }
+        }
+
+        private bool favorite;
+        public bool Favorite
+        {
+            get { return favorite; }
+            set { favorite = value; }
+        }
+
+
+
+    }
+}
